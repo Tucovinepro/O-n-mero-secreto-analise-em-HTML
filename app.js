a3b69aa4138b8af0824f893670206f1688cbcecf
Tucovinegit <20120032@ccpa.com.br>
2026-03-09 19:50:13 -0300

    Fixing the problem with Math.random

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-let numerosecreto = gerarnumerosecreto();
+let numerosecreto = gerarNumeroSecreto();
 
 function exibirtextonatela(tag, texto){
 let campo = document.querySelector(tag)
  campo.innerHTML = texto
-
 }
 
 exibirtextonatela('h1', "Jogo do número secreto")
-exibirtextonatela('p',"Escolha um número entre 1 e 100" )
+exibirtextonatela('p',"Escolha um número entre 1 e 10" )
 
 function verificarChute() {
     console.log(numerosecreto);
 }
 
-function gerarnumerosecreto(){
-   return parseInt(Math.random() * 10 + 1)
+function gerarNumeroSecreto(){
+   return parseInt(Math.random()*10+1)
     
 }
